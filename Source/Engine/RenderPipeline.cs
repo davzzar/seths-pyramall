abbccc37d9b94520385c4cfc449612a7c9deda25
Jasper Ermatinger <chillersanim@gmail.com>
2022-04-11 00:50:34 +0200

    Fixed renderer depth value that was inverted.

--- Source/Engine/RenderPipeline.cs
+++ Source/Engine/RenderPipeline.cs
@@ -149,7 +149,7 @@ namespace Engine
                 // Draw scene
                 if (this.renderers.Count > 0)
                 {
-                    Graphics.SpriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend, SamplerState.PointClamp);
+                    Graphics.SpriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend, SamplerState.PointClamp);
                     
                     foreach (var renderer in this.renderers)
                     {
