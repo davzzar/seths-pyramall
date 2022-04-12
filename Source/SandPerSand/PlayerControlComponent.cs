b620681a9ad56757efa948c46837667fb64408e0
David Simon Tetruashvii <dtetruash@student.ethz.ch>
2022-04-12 15:24:52 +0200

    Add input control indicator for debugging

--- Source/SandPerSand/PlayerControlComponent.cs
+++ Source/SandPerSand/PlayerControlComponent.cs
@@ -8,12 +8,10 @@ namespace SandPerSand
     internal class PlayerControlComponent : Behaviour
     {
         /// <summary>
-        /// This component is responsible for interpreting GamePad input and updating the player accordingly.
+        /// This component is responsible for player control.
         /// </summary>
         private InputHandler inputHandler;
-
         private RigidBody playerRB;
-        private Vector2[] dubugPlayerColliderOutline;
         private GroundCheckComponent groundChecker;
 
         private const float JumpForce = 10.0f;
@@ -36,13 +34,13 @@ namespace SandPerSand
             inputHandler = new InputHandler(this.PlayerIndex);
             playerRB = this.Owner.GetComponent<RigidBody>();
             groundChecker = this.Owner.GetComponent<GroundCheckComponent>();
-            dubugPlayerColliderOutline = this.Owner.GetComponent<PolygonCollider>().Outline;
 
             this.Owner.Layer = 1;
         }
 
         protected override void Update()
         {
+            DrawInputControls();
             //impulse up if A is pressed
             switch (inputHandler.getButtonState(Buttons.A))
             {
@@ -70,6 +68,42 @@ namespace SandPerSand
 
             // Update the input handler's state
             inputHandler.UpdateState();
+        }
+
+        /// <summary>
+        /// Draws the current right analogue stick and jump button inputs next to player
+        /// for debuging purposes.
+        /// </summary>
+        private void DrawInputControls()
+        {
+            var pos = this.Transform.Position;
+            var stickLineOrigin = pos + (-Vector2.UnitX + Vector2.UnitY);
+
+            var stickDir = inputHandler.getLeftThumbstickDir(magnitudeThreshold: 0f);
+            Gizmos.DrawRect(stickLineOrigin, 0.5f * Vector2.One, Color.Black);
+            Gizmos.DrawLine(stickLineOrigin, stickLineOrigin + stickDir, Color.Black);
+
+            
+            var jumpButtonState = inputHandler.getButtonState(Buttons.A);
+            Color jumpIndicatorColor;
+            switch (jumpButtonState)
+            {
+                case ButtonState.Pressed:
+                    jumpIndicatorColor = Color.Yellow;
+                    break;
+                case ButtonState.Held:
+                    jumpIndicatorColor = Color.Red;
+                    break;
+                case ButtonState.Released:
+                    jumpIndicatorColor = Color.Magenta;
+                    break;
+                default:
+                    jumpIndicatorColor = Color.Black;
+                    break;
+            }
+            var jumpButtonOrigin = pos + Vector2.UnitY;
+            Gizmos.DrawRect(jumpButtonOrigin, 0.5f * Vector2.One, jumpIndicatorColor);
+
         }
     }
 }
