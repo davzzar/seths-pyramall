8da412a0c91db6d9b103a8fa871dec349eb4fc6d
baclemen <clemens.s.bachmann@gmail.com>
2022-04-25 16:33:48 +0200

    add faster animation. adjust level update colors

--- Source/SandPerSand/Content/tiles/PlayerAnimated4.tsx
+++ Source/SandPerSand/Content/tiles/PlayerAnimated4.tsx
@@ -8,38 +8,74 @@
    <property name="AnimName" value="Spawn"/>
   </properties>
   <animation>
-   <frame tileid="0" duration="150"/>
-   <frame tileid="1" duration="150"/>
-   <frame tileid="2" duration="150"/>
-   <frame tileid="3" duration="150"/>
-   <frame tileid="4" duration="150"/>
-   <frame tileid="5" duration="150"/>
-   <frame tileid="6" duration="150"/>
-   <frame tileid="7" duration="150"/>
-   <frame tileid="8" duration="150"/>
-   <frame tileid="9" duration="150"/>
-   <frame tileid="10" duration="150"/>
-   <frame tileid="11" duration="150"/>
-   <frame tileid="12" duration="150"/>
-   <frame tileid="13" duration="150"/>
-   <frame tileid="14" duration="150"/>
-   <frame tileid="15" duration="150"/>
-   <frame tileid="16" duration="150"/>
-   <frame tileid="17" duration="150"/>
-   <frame tileid="18" duration="150"/>
-   <frame tileid="19" duration="150"/>
-   <frame tileid="20" duration="150"/>
-   <frame tileid="21" duration="150"/>
-   <frame tileid="22" duration="150"/>
-   <frame tileid="23" duration="150"/>
-   <frame tileid="24" duration="150"/>
-   <frame tileid="25" duration="150"/>
-   <frame tileid="26" duration="150"/>
-   <frame tileid="27" duration="150"/>
-   <frame tileid="28" duration="150"/>
-   <frame tileid="29" duration="150"/>
-   <frame tileid="30" duration="150"/>
-   <frame tileid="31" duration="150"/>
+   <frame tileid="0" duration="30"/>
+   <frame tileid="1" duration="30"/>
+   <frame tileid="2" duration="30"/>
+   <frame tileid="3" duration="30"/>
+   <frame tileid="4" duration="30"/>
+   <frame tileid="5" duration="30"/>
+   <frame tileid="6" duration="30"/>
+   <frame tileid="7" duration="30"/>
+   <frame tileid="8" duration="30"/>
+   <frame tileid="9" duration="30"/>
+   <frame tileid="10" duration="30"/>
+   <frame tileid="11" duration="30"/>
+   <frame tileid="12" duration="30"/>
+   <frame tileid="13" duration="30"/>
+   <frame tileid="14" duration="30"/>
+   <frame tileid="15" duration="30"/>
+   <frame tileid="16" duration="30"/>
+   <frame tileid="17" duration="30"/>
+   <frame tileid="18" duration="30"/>
+   <frame tileid="19" duration="30"/>
+   <frame tileid="20" duration="30"/>
+   <frame tileid="21" duration="30"/>
+   <frame tileid="22" duration="30"/>
+   <frame tileid="23" duration="30"/>
+   <frame tileid="24" duration="30"/>
+   <frame tileid="25" duration="30"/>
+   <frame tileid="26" duration="30"/>
+   <frame tileid="27" duration="30"/>
+   <frame tileid="28" duration="30"/>
+   <frame tileid="29" duration="30"/>
+   <frame tileid="30" duration="30"/>
+   <frame tileid="31" duration="30"/>
+  </animation>
+ </tile>
+ <tile id="31">
+  <animation>
+   <frame tileid="0" duration="30"/>
+   <frame tileid="1" duration="30"/>
+   <frame tileid="2" duration="30"/>
+   <frame tileid="3" duration="30"/>
+   <frame tileid="4" duration="30"/>
+   <frame tileid="5" duration="30"/>
+   <frame tileid="6" duration="30"/>
+   <frame tileid="7" duration="30"/>
+   <frame tileid="8" duration="30"/>
+   <frame tileid="9" duration="30"/>
+   <frame tileid="10" duration="30"/>
+   <frame tileid="11" duration="30"/>
+   <frame tileid="12" duration="30"/>
+   <frame tileid="13" duration="30"/>
+   <frame tileid="14" duration="30"/>
+   <frame tileid="15" duration="30"/>
+   <frame tileid="16" duration="30"/>
+   <frame tileid="17" duration="30"/>
+   <frame tileid="18" duration="30"/>
+   <frame tileid="19" duration="30"/>
+   <frame tileid="20" duration="30"/>
+   <frame tileid="21" duration="30"/>
+   <frame tileid="22" duration="30"/>
+   <frame tileid="23" duration="30"/>
+   <frame tileid="24" duration="30"/>
+   <frame tileid="25" duration="30"/>
+   <frame tileid="26" duration="30"/>
+   <frame tileid="27" duration="30"/>
+   <frame tileid="28" duration="30"/>
+   <frame tileid="29" duration="30"/>
+   <frame tileid="30" duration="30"/>
+   <frame tileid="31" duration="30"/>
   </animation>
  </tile>
  <tile id="32">
