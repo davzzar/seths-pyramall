b20cb55bb256df5b4c7bb8129da577191d01769f
Jasper Ermatinger <chillersanim@gmail.com>
2022-04-25 12:08:42 +0200

    Simple camera controller

---
+++ Source/SandPerSand/CameraController2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Engine;
+using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
+
+namespace SandPerSand
+{
+    public sealed class CameraController2 : Behaviour
+    {
+        private static readonly List<CameraControlPoint> controlPoints = new List<CameraControlPoint>();
+
+        private Camera camera;
+
+        public Vector2 MinCameraSize { get; set; } = Vector2.One * 5;
+
+        public static void AddControlPoint([NotNull]CameraControlPoint controlPoint)
+        {
+            if (controlPoint == null)
+            {
+                throw new ArgumentNullException(nameof(controlPoint));
+            }
+
+            Debug.Assert(!controlPoints.Contains(controlPoint));
+            controlPoints.Add(controlPoint);
+        }
+
+        public static void RemoveControlPoint([NotNull] CameraControlPoint controlPoint)
+        {
+            if (controlPoint == null)
+            {
+                throw new ArgumentNullException(nameof(controlPoint));
+            }
+
+            controlPoints.Remove(controlPoint);
+        }
+
+        protected override void OnEnable()
+        {
+            this.camera = this.Owner.GetOrAddComponent<Camera>();
+        }
+
+        protected override void Update()
+        {
+            if (this.camera == null)
+            {
+                return;
+            }
+
+            if (controlPoints.Count == 0)
+            {
+                return;
+            }
+
+            var min = new Vector2(float.PositiveInfinity);
+            var max = new Vector2(float.NegativeInfinity);
+
+            foreach (var controlPoint in controlPoints)
+            {
+                var pos = controlPoint.Transform.Position;
+
+                if (controlPoint.AffectsHorizontal)
+                {
+                    min.X = MathF.Min(min.X, pos.X);
+                    max.X = MathF.Max(max.X, pos.X);
+                }
+
+                if (controlPoint.AffectsVertical)
+                {
+                    min.Y = MathF.Min(min.Y, pos.Y);
+                    max.Y = MathF.Max(max.Y, pos.Y);
+                }
+            }
+
+            if (max.X < min.X)
+            {
+                min.X = 0f;
+                max.X = 0f;
+            }
+
+            if (max.Y < min.Y)
+            {
+                min.Y = 0f;
+                max.Y = 0f;
+            }
+
+            var center = (min + max) / 2f;
+            var size = Vector2.Max(max - min, this.MinCameraSize);
+            var aspectRatio = this.camera.AspectRatio;
+
+            this.camera.Transform.Position = center;
+            this.camera.Height = MathF.Max(size.X, size.Y * aspectRatio);
+        }
+    }
+
+    public sealed class CameraControlPoint : Component
+    {
+        public bool AffectsHorizontal { get; set; } = true;
+
+        public bool AffectsVertical { get; set; } = true;
+
+        /// <inheritdoc />
+        protected override void OnAwake()
+        {
+            CameraController2.AddControlPoint(this);
+        }
+
+        /// <inheritdoc />
+        protected override void OnDestroy()
+        {
+            CameraController2.RemoveControlPoint(this);
+        }
+    }
+}
