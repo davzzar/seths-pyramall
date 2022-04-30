d6ed00e7a34bb0c36df59ac87ed2300604f01097
Todor Hristov <thristov@student.ethz.ch>
2022-04-30 22:08:47 +0200

    DO NOT MERGE:: Auto-start of sand sim

--- Source/SandPerSand/GameStateManager.cs
+++ Source/SandPerSand/GameStateManager.cs
@@ -48,8 +48,8 @@ namespace SandPerSand
                 if (instance == null)
                 {
                     instance = new GameStateManager();
-                    currentState = GameState.Prepare;
-                    GraphicalUserInterface.Instance.renderMidScreenText("To Start The Game Press A");
+                    currentState = GameState.InRound;
+                    GraphicalUserInterface.Instance.renderMidScreenText("SandSim started automatically. Also fuck ");
                 }
                 return instance;
             }
