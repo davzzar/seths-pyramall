5b1e3fdc8b10803b9485a114ae815a82fef64d0b
David Simon Tetruashvii <dtetruash@student.ethz.ch>
2022-05-02 16:26:37 +0200

    Imgrate to use Template to make players
    Remove previous player creation function, refactor component creation order, replace Sprite renderer with animation controller

--- Source/SandPerSand/Templates/PlayerTemplate.cs
+++ Source/SandPerSand/Templates/PlayerTemplate.cs
@@ -6,13 +6,19 @@ namespace SandPerSand
 {
     public static partial class Template
     {
+        /// <summary>
+        /// Create and instantiate a player game object from template.
+        /// </summary>
+        /// <param name="playerIndex">Index of the layer to be created</param>
+        /// <param name="position">Vector position for the player to be created</param>
+        /// <returns></returns>
         public static GameObject MakePlayer(PlayerIndex playerIndex, Vector2 position)
         {
             var playerGo = new GameObject($"Player {playerIndex}");
             playerGo.Transform.LocalPosition = position;
 
             var playerRenderer = playerGo.AddComponent<SpriteRenderer>();
-
+                
             string spritePath = playerIndex switch
             {
                 (PlayerIndex.One) => "sprite_player_one",
