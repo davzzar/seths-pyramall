8adbf23285f47f3c08dd3fc12f20293de4612f5e
David Simon Tetruashvii <dtetruash@student.ethz.ch>
2022-05-03 16:14:56 +0200

    Make Player Component to manage a player game object

---
+++ Source/SandPerSand/PlayerComponent.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using Engine;
+using Microsoft.Xna.Framework;
+
+namespace SandPerSand
+{
+    public class PlayerComponent : Behaviour
+    {
+        private PlayerIndex playerIndex;
+
+        public PlayerIndex PlayerIndex
+        {
+            get => playerIndex;
+            set
+            {
+                if (!Enum.IsDefined(typeof(PlayerIndex), value))
+                    throw new InvalidEnumArgumentException(nameof(value), (int) value, typeof(PlayerIndex));
+                // Update the InputHandler when the player index is updated
+                
+                if (value != playerIndex) InputHandler = new InputHandler(value);
+                playerIndex = value;
+            }
+        }
+
+        public InputHandler InputHandler { get; set; }
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+
+            InputHandler = new InputHandler(PlayerIndex);
+            Debug.Print($"Player with player index {PlayerIndex.ToString()} created");
+
+            var playerCollider = Owner.AddComponent<CircleCollider>();
+            playerCollider.Radius = 1f;
+            playerCollider.Friction = 0.0f;
+
+            var playerRB = Owner.AddComponent<RigidBody>();
+            playerRB.IsKinematic = false;
+            playerRB.FreezeRotation = true;
+            playerRB.IgnoreGravity = true;
+
+            Owner.AddComponent<GroundCheckComponent>();
+
+            var controlComp = Owner.AddComponent<PlayerControlComponent>();
+            controlComp.InputHandler = InputHandler;
+
+            var playerStates = Owner.AddComponent<PlayerStates>();
+            playerStates.InputHandler = InputHandler;
+
+            // animator need to be created after controlComp and input handler
+            var playerAnimator = Owner.AddComponent<Animator>();
+
+            string animationTexture = PlayerIndex switch
+            {
+                (PlayerIndex.One) => "PlayerAnimated",
+                (PlayerIndex.Two) => "PlayerAnimated2",
+                (PlayerIndex.Three) => "PlayerAnimated3",
+                (PlayerIndex.Four) => "PlayerAnimated4",
+                _ => ""
+            };
+
+            playerAnimator.LoadFromContent("PlayerAnimated", animationTexture);
+            Owner.AddComponent<MyAnimatorController>();
+
+            //FOR DEBUG (updated in the PlayerControlComponent)
+            var textRenderer = Owner.AddComponent<GuiTextRenderer>();
+            textRenderer.ScreenPosition = Vector2.UnitY * 100f;
+            var tracer = Owner.AddComponent<TracerRendererComponent>();
+            tracer.TraceLength = 60;
+
+            var cameraControlPoint = Owner.AddComponent<CameraControlPoint>();
+            cameraControlPoint.Margin = new Border(5, 10, 5, 5);
+        }
+    }
+}
--- Source/SandPerSand/Templates/PlayerTemplate.cs
+++ Source/SandPerSand/Templates/PlayerTemplate.cs
@@ -14,53 +14,11 @@ namespace SandPerSand
         /// <returns></returns>
         public static GameObject MakePlayer(PlayerIndex playerIndex, Vector2 position)
         {
-            var inputHandler = new InputHandler(playerIndex);
-            Debug.Print("player with player index created");
-            Debug.Print(playerIndex.ToString());
-
             var playerGo = new GameObject($"Player {playerIndex}");
+            var playerComponent = playerGo.AddComponent<PlayerComponent>();
+            playerComponent.PlayerIndex = playerIndex;
+
             playerGo.Transform.LocalPosition = position;
-
-            var playerCollider = playerGo.AddComponent<CircleCollider>();
-            playerCollider.Radius = 1f;
-            playerCollider.Friction = 0.0f;
-
-            var playerRB = playerGo.AddComponent<RigidBody>();
-            playerRB.IsKinematic = false;
-            playerRB.FreezeRotation = true;
-            playerRB.IgnoreGravity = true;
-
-            playerGo.AddComponent<GroundCheckComponent>();
-
-            var controlComp = playerGo.AddComponent<PlayerControlComponent>();
-            controlComp.InputHandler = inputHandler;
-
-            var playerStates = playerGo.AddComponent<PlayerStates>();
-            playerStates.InputHandler = inputHandler;
-
-            // animator need to be created after controlComp and input handler
-            var playerAnimator = playerGo.AddComponent<Animator>();
-             
-            string animationTexture = playerIndex switch
-            {
-                (PlayerIndex.One) => "PlayerAnimated",
-                (PlayerIndex.Two) => "PlayerAnimated2",
-                (PlayerIndex.Three) => "PlayerAnimated3",
-                (PlayerIndex.Four) => "PlayerAnimated4",
-                _=> ""
-            };
-            
-            playerAnimator.LoadFromContent("PlayerAnimated", animationTexture);
-            playerGo.AddComponent<MyAnimatorController>();
-
-            //FOR DEBUG (updated in the PlayerControlComponent)
-            var textRenderer = playerGo.AddComponent<GuiTextRenderer>();
-            textRenderer.ScreenPosition = Vector2.UnitY * 100f;
-            var tracer = playerGo.AddComponent<TracerRendererComponent>();
-            tracer.TraceLength = 60;
-
-            var cameraControlPoint = playerGo.AddComponent<CameraControlPoint>();
-            cameraControlPoint.Margin = new Border(5, 10, 5, 5);
 
             return playerGo;
         }
