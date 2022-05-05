4fbca726ba680d85ce4b5a71cc49422546e078bb
whostheme <2727yuc@gmail.com>
2022-05-06 00:46:08 +0200

    Add ShopManager draft

---
+++ Source/SandPerSand/Shop/ShopManager.cs
@@ -0,0 +1,34 @@
+﻿using Engine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+
+namespace SandPerSand
+{
+    public class ShopManager : Behaviour
+    {
+        // optional: randomly generate shopping list
+
+        // accept rank as initial parameters
+        // create players in queue
+        // disable all players controller
+        // enable the first player's controller
+        // disable the current player's controller after they finished shopping or after the time limit
+        // move the finished player to the exit if they is not there
+        // enable the next first player's controller
+        // ...
+        // after all player is moved to exit, proceed to the next round
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+
+
+    }
+}
